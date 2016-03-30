42a1afc3f35bd73ea03b3b7cf9003e9257524daa
spmbob <urban_manki@mail.ru>
2016-03-30 20:55:15 +0300

    Laba123

---
+++ main.js
@@ -0,0 +1,8 @@
+(function() {
+var div = document.getElementsByClassName('lll');
+ style = div[0].style;
+ style.color = "black";
+ style.backgroundColor = "#245667";
+ style.padding = "70px";
+ style.fontSize = "37px";
+ })();
